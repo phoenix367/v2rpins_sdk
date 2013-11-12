9616391b3d44177960c35cbfed1dfe6611c0c91a
Ivan Gubochkin <jhng@yandex.ru>
2013-11-12 23:22:34 +0400

    Added communication server stub for Cherokey 4WD robot project

---
+++ cherokey-robot/commserver/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Ivan Gubochkin
+ *
+ * Created on November 12, 2013, 11:05 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ cherokey-robot/commserver/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=V2R-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/V2R-Linux-x86
+CND_ARTIFACT_NAME_Debug=commserver
+CND_ARTIFACT_PATH_Debug=dist/Debug/V2R-Linux-x86/commserver
+CND_PACKAGE_DIR_Debug=dist/Debug/V2R-Linux-x86/package
+CND_PACKAGE_NAME_Debug=commserver.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/V2R-Linux-x86/package/commserver.tar
+# Release configuration
+CND_PLATFORM_Release=V2R-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/V2R-Linux-x86
+CND_ARTIFACT_NAME_Release=commserver
+CND_ARTIFACT_PATH_Release=dist/Release/V2R-Linux-x86/commserver
+CND_PACKAGE_DIR_Release=dist/Release/V2R-Linux-x86/package
+CND_PACKAGE_NAME_Release=commserver.tar
+CND_PACKAGE_PATH_Release=dist/Release/V2R-Linux-x86/package/commserver.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
