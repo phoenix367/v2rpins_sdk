0d2d89e41843a7bc490bf5566010f9c340f04135
Ivan Gubochkin <jhng@yandex.ru>
2014-03-18 01:44:25 +0400

    Stub for mono slam library

---
+++ cherokey-robot/mono_slam/include/mono_slam/mono_slam.hpp
@@ -0,0 +1,13 @@
+/* 
+ * File:   mono_slam.hpp
+ * Author: Ivan Gubochkin
+ *
+ * Created on 18 Март 2014 г., 1:42
+ */
+
+#ifndef MONO_SLAM_HPP
+#define	MONO_SLAM_HPP
+
+
+
+#endif	/* MONO_SLAM_HPP */
